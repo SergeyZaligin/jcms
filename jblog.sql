e546796b4f4c0b385ee5b3162fef1d09c740cfda
cobweb <codename.cobweb@gmail.com>
2018-05-27 15:00:33 +0300

    first commit

---
+++ jblog.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 27, 2018 at 01:59 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+CREATE TABLE `author` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`id`, `name`, `email`) VALUES
+(1, 'Кевин Янк', 'thatguy@kevinyank.com'),
+(2, 'Джоан Смит', 'joan@example.com'),
+(3, 'Сергей Залыгин', 'serg@example.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'О д\'Артаньяне'),
+(2, 'Через дорогу'),
+(3, 'Об адвокатах'),
+(4, 'Новый год');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `joke`
+--
+
+CREATE TABLE `joke` (
+  `id` int(11) NOT NULL,
+  `joketext` text,
+  `jokedate` date NOT NULL,
+  `authorid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `joke`
+--
+
+INSERT INTO `joke` (`id`, `joketext`, `jokedate`, `authorid`) VALUES
+(1, 'Why did the chicken cross the road? To get to the other side!', '2018-05-26', 3),
+(2, 'Knock-knock! Who\'s there? Boo! \"Boo\" who? Don\'t cry; it\'s only a joke!', '2012-04-01', 3),
+(5, '				Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. ', '2018-05-27', 3),
+(6, '				Эта парадигматическая страна, в которой жаренные члены предложения залетают прямо в рот.', '2018-05-27', 3),
+(7, '				Душа моя озарена неземной радостью, как эти чудесные весенние утра, которыми я наслаждаюсь от всего сердца. ', '2018-05-27', 3),
+(10, 'Зачем цыпленок перешел дорогу? Чтобы попасть на другую\r\nсторону!', '2012-04-01', 1),
+(11, '\"Королеве - подвески, Констанции - подвязки, Портосу - подтяжки...\" -\r\nповторял, чтобы не перепутать, д\'Артаньян по дороге в Англию.', '2012-04-01', 1),
+(12, 'В Новый год все сбывается, даже то, что в другое время сбыть не\r\nудается.\"', '2012-04-01', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jokecategory`
+--
+
+CREATE TABLE `jokecategory` (
+  `jokeid` int(11) NOT NULL,
+  `categoryid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `jokecategory`
+--
+
+INSERT INTO `jokecategory` (`jokeid`, `categoryid`) VALUES
+(1, 2),
+(2, 1),
+(3, 4),
+(4, 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `joke`
+--
+ALTER TABLE `joke`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jokecategory`
+--
+ALTER TABLE `jokecategory`
+  ADD PRIMARY KEY (`jokeid`,`categoryid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `author`
+--
+ALTER TABLE `author`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `joke`
+--
+ALTER TABLE `joke`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
